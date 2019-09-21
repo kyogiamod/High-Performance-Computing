c755c95fc855c629860be5e5ed327062b7e278d1
Richard Torti <richard.torti@usach.cl>
2019-09-21 07:00:17 -0300

    Test passed

--- Lab1/main.c
+++ Lab1/main.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "fileDriver.h"
 #include "dilation.h"
 #include "miscellaneous.h"
 
 int main(){
 
-    int filas = 6;
+    int filas = 8;
     int** in = newMatrix(filas);
     int read = readFile("test/my64int.raw", in);
     
+    int** outSeq = newMatrix(filas);
+    sequential_dilation(in, outSeq, filas);
+    saveFile("test/my64intoutsequential.raw", outSeq, filas);
+
+    float** in2 = int_to_float(in, filas);
+    float** outSIMD = (float**)malloc(sizeof(float*)*filas);
     int i,j;
+    for(i = 0; i < filas; i++){ outSIMD[i] = (float*)malloc(sizeof(float)*filas); }
+
+    simd_dilation(in2, outSIMD, filas);
+
+    int** toInt = float_to_int(outSIMD, filas);
+
+    saveFile("test/my64intout.raw", toInt, filas);
     
-	showFigure(in, filas);
-
-    printf("\n");
-    //Testing out
-    saveFile("test/out.raw", in, filas);
-
-    int** in2 = newMatrix(filas);
-    int read2 = readFile("test/out.raw", in2);
-
-    showFigure(in2, filas);
-
-
     return 0;
 }
