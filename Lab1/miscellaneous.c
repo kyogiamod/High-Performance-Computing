b31df8f0f99d1ef334e28876731b598c53c1d92e
Richard Torti <richard.torti@usach.cl>
2019-09-21 06:58:31 -0300

    Adding functions to use SIMD

--- Lab1/miscellaneous.c
+++ Lab1/miscellaneous.c
@@ -31,3 +31,43 @@ void showFigure(int** img, int length){
 		printf("\n");
     }
 }
+
+float** int_to_float(int** in, int rows){
+
+    float** out = (float**)malloc(sizeof(float*)*rows);
+    
+    int i,j;
+    for(i = 0; i < rows; i++){
+        out[i] = (float*)malloc(sizeof(float)*rows);
+        for(j = 0; j < rows; j++){
+            out[i][j] = (float)in[i][j];
+        }
+    }
+    return out;
+}
+
+void showFigureFloat(float** img, int length){
+
+    int i,j;
+	for (i = 0; i < length; i++)
+	{
+        printf("%d: ", i);
+        for(j = 0; j < length; j++){
+            printf("%d ", img[i][j]==0?0:1);
+        }
+		printf("\n");
+    }
+}
+
+int** float_to_int(float**in, int length){
+
+    int** out = newMatrix(length);
+    int i,j;
+    for(i = 0; i < length; i++){
+        for(j = 0; j < length; j++){
+            out[i][j] = (int)in[i][j];
+        }
+    }
+
+    return out;
+}
--- Lab1/miscellaneous.h
+++ Lab1/miscellaneous.h
@@ -5,6 +5,8 @@
 int** newMatrix(int rows);
 int max(int up, int down, int left, int right);
 void showFigure(int** img, int length);
-
+float** int_to_float(int** in, int rows);
+void showFigureFloat(float** img, int length);
+int** float_to_int(float**in, int length);
 
 #endif
